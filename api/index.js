552a51f9ae078308871e736d2f6964317e1f7740
Surendra9129 <91534930+Surendra9129@users.noreply.github.com>
2022-01-25 23:21:13 +0530

    created complete api for backend

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const app=express();
 
 const authRout=require('./routes/auth')
 const userRoute=require('./routes/users')
+const movieRoute=require('./routes/movies');
+const listRoute=require('./routes/lists');
 const dotenv=require('dotenv');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL,{
@@ -15,7 +17,8 @@ app.use(express.json())
 
 app.use('/api/auth',authRout);
 app.use('/api/users',userRoute);
-
+app.use('/api/movies',movieRoute);
+app.use('/api/lists',listRoute);
 
 app.listen(8800,()=>{
     console.log('backend server is running');
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const User=require('../models/User');
-const CryptoJS=require('crypto-js');
+const CryptoJS = require("crypto-js");
 const verify=require('../verifyToken')
 //UPDATE
  router.put('/:id',verify,async(req,res)=>{
@@ -12,6 +12,7 @@ const verify=require('../verifyToken')
                   process.env.SECRET_KEY
                   ).toString();
          }
+
          try{
         const updateUser= await User.findByIdAndUpdate(req.params.id,{
             $set: req.body,
@@ -25,5 +26,60 @@ const verify=require('../verifyToken')
          res.status(403).json('you can update only your account')
      }
  })
+
+
+ //Delete
+
+ router.delete('/:id',verify,async(req,res)=>{
+    if(req.user.id===req.params.id || req.user.isAdmin){
+   
+
+        try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("user has been deleted");
+
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json('you can delete only your account')
+    }
+})
+
+//Get
+
+
+router.get('/find/:id',async(req,res)=>{
+    
+   
+
+        try{
+       const user= await User.findById(req.params.id);
+       const {password, ...info}= user._doc;
+        res.status(200).json(info);
+
+        }catch(err){
+            res.status(500).json(err);
+        }
+   })
+
+   // All user
+
+   router.get('/',verify,async(req,res)=>{
+       const query=req.query.new
+    if(req.user.isAdmin){
+   
+
+        try{
+         const users=query ? await User.find().limit(10): await User.find();
+        res.status(200).json(users);
+
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json('you can delete only your account')
+    }
+})
 
  module.exports=router;
